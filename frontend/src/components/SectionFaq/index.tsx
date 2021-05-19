1e5c3aaaa3d21ef93fed951e4d6fe0631c532640
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-05-19 19:59:09 -0300

    feat: update section components

--- frontend/src/components/SectionFaq/index.tsx
+++ frontend/src/components/SectionFaq/index.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 import Container from 'components/Container'
 import Heading from 'components/Heading'
 
-import faq from './content'
 import * as S from './styles'
+import { SectionFaqProps } from 'types/api'
 
-const SectionFaq = () => (
+const SectionFaq = ({ title, questions }: SectionFaqProps) => (
   <S.Wrapper>
     <S.Content>
       <Container>
-        <Heading>FAQ</Heading>
+        <Heading>{title}</Heading>
 
         <S.Questions>
-          {faq.map(({ question, answer }, index) => (
+          {questions.map(({ question, answer }, index) => (
             <S.Question key={index}>
               <Heading lineBottom>{question}</Heading>
               <div dangerouslySetInnerHTML={{ __html: answer }} />
@@ -26,14 +26,18 @@ const SectionFaq = () => (
           <p>
             Sem problemas! Você pode acessar qualquer uma das{' '}
             <a
-              href="https://willianjusten.com.br/about"
+              href="https://willianjusten.com/about"
               target="_blank"
               rel="noreferrer"
             >
               minhas redes sociais
             </a>{' '}
             ou entrar no{' '}
-            <a href="http://bit.ly/slack-will" target="_blank" rel="noreferrer">
+            <a
+              href="https://slack-willianjusten.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
               slack do nosso curso.
             </a>
           </p>
